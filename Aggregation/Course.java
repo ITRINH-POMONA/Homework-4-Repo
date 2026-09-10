57573476e5766586719100f230465fa360edcb31
ITRINH-POMONA <itrinh@cpp.edu>
2026-09-09 22:11:43 -0700

    Modifying Part 4
    Adding two instructors and textbooks as standard for course class

--- Aggregation/Course.java
+++ Aggregation/Course.java
@@ -1,12 +1,16 @@
 public class Course {
     private String name;
-    private Instructor instructor;
-    private Textbook textbook;
+    private Instructor instructor1;
+    private Instructor instructor2;
+    private Textbook textbook1;
+    private Textbook textbook2;
 
-    public Course(String name, Instructor instructor, Textbook textbook) {
+    public Course(String name, Instructor instructor1, Instructor instructor2, Textbook textbook1, Textbook textbook2) {
         this.name = name;
-        this.instructor = instructor;
-        this.textbook = textbook;
+        this.instructor1 = instructor1;
+        this.instructor2 = instructor2;
+        this.textbook1 = textbook1;
+        this.textbook2 = textbook2;
     }
 
     // setters
@@ -14,34 +18,55 @@ public class Course {
         this.name = name;
     }
  
-    public void setInstructor(Instructor instructor) {
-        this.instructor = instructor;
+    public void setInstructor1(Instructor instructor1) {
+        this.instructor1 = instructor1;
     }
  
-    public void setTextbook(Textbook textbook) {
-        this.textbook = textbook;
+    public void setInstructor2(Instructor instructor2) {
+        this.instructor2 = instructor2;
     }
+ 
+    public void setTextbook1(Textbook textbook1) {
+        this.textbook1 = textbook1;
+    }
+ 
+    public void setTextbook2(Textbook textbook2) {
+        this.textbook2 = textbook2;
+    }
+
 
     // getters
     public String getName() {
         return name;
     }
  
-    public Instructor getInstructor() {
-        return instructor;
+    public Instructor getInstructor1() {
+        return instructor1;
     }
  
-    public Textbook getTextbook() {
-        return textbook;
+    public Instructor getInstructor2() {
+        return instructor2;
+    }
+ 
+    public Textbook getTextbook1() {
+        return textbook1;
+    }
+ 
+    public Textbook getTextbook2() {
+        return textbook2;
     }
 
-    
+
     public void print() {
         System.out.println("Course: " + getName());
-        System.out.println("Instructor: " + instructor.getFirstName()
-                + " " + instructor.getLastName());
-        System.out.println("Textbook: \"" + textbook.getTitle()
-                + "\" by " + textbook.getAuthor());
+        System.out.println("Instructor 1: " + instructor1.getFirstName()
+                + " " + instructor1.getLastName());
+        System.out.println("Instructor 2: " + instructor2.getFirstName()
+                + " " + instructor2.getLastName());
+        System.out.println("Textbook 1: \"" + textbook1.getTitle()
+                + "\" by " + textbook1.getAuthor());
+        System.out.println("Textbook 2: \"" + textbook2.getTitle()
+                + "\" by " + textbook2.getAuthor());
     }
 
 }
--- Aggregation/CourseRunner.java
+++ Aggregation/CourseRunner.java
@@ -1,15 +1,15 @@
 public class CourseRunner {
     public static void main(String[] args) {
-        Instructor instructor = new Instructor("Nima", "Davarpanah", "3-2636");
- 
-        // Textbook: "Clean Code" (author/publisher made up, as instructed)
-        Textbook textbook = new Textbook("Clean Code", "Robert C. Martin", "Prentice Hall");
- 
-        // Course
-        Course course = new Course("Advanced Software Engineering", instructor, textbook);
- 
-        // Display the course information
-        course.print();
-    }
 
+        Instructor instructor1 = new Instructor("Nima", "Davarpanah", "3-2636");
+        Instructor instructor2 = new Instructor("Jeff", "Jones", "4-1187");
+ 
+        Textbook textbook1 = new Textbook("Clean Code", "Robert C. Martin", "Prentice Hall");
+        Textbook textbook2 = new Textbook("Design Patterns Elements of Reusable Object-Oriented Software", "Erich Gamma & co", "Addison-Wesley");
+ 
+        Course course = new Course("Advanced Software Engineering", instructor1, instructor2, textbook1, textbook2);
+ 
+        course.print();
+
+    }
 }
